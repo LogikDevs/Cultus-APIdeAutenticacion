626addb092dbefba6d20551e7772d811adfe1dbd
ChinoMandarin <francofedullo22@gmail.com>
2023-07-04 20:44:25 -0300

    Login terminado

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -5,9 +5,15 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use Illuminate\Support\Str;
 class UserTest extends TestCase
 {
+        private $clientId = 100;
+        private $clientSecret = "wsBa0mp4jwSTYssUGHX5xoqD9IC0X95Gfpg0w3uY";
+
+        private $userName = "usuario@email.com";
+        private $userPassword = "12345678";
+
 
     public function test_ListOneThatExist(){
         $response = $this->get('/api/v1/user/1');
@@ -138,5 +144,113 @@ class UserTest extends TestCase
                     "homeland"=>["The homeland must be an integer."],
                     "residence"=> ["The selected residence is invalid."]
             ]);
+        }
+
+        public function test_loginGoodRequest(){
+            $response = $this->post('/oauth/token',[
+                "username" => $this -> userName,
+                "password" => $this -> userPassword,
+                "grant_type" => "password",
+                "client_id" => $this -> clientId,
+                "client_secret" => $this -> clientSecret
+            ]);
+
+            $response->assertStatus(200);
+
+            $response->assertJsonStructure([
+                "token_type",
+                "expires_in",
+                "access_token",
+                "refresh_token"
+            ]);
+
+            $response->assertJsonFragment([
+                "token_type" => "Bearer"
+            ]);
+        }
+        public function test_loginBadRequest(){
+            $response = $this->post('/oauth/token',[
+                "grant_type" => "password",
+                "client_id" => "234",
+                "client_secret" => Str::Random(8)
+            ]);
+
+            $response->assertStatus(401);
+    
+            $response->assertJsonFragment([
+                "error" => "invalid_client",
+                "error_description" => "Client authentication failed",
+                "message" => "Client authentication failed"
+            ]);
+        }
+
+        public function test_ValidateNoToken(){    
+            $response = $this->get('/api/v1/validate');
+
+            $response->assertStatus(500);
+        }
+
+        public function test_ValidateBadRequest(){
+            $response = $this->get('/api/v1/validate',[
+                [ "Authorization" => "Bearer " . Str::Random(40)]
+            ]);
+    
+            $response->assertStatus(500);
+        }
+
+        public function test_ValidateGoodRequest(){
+            $tokenResponse = $this->post('/oauth/token',[
+                "username" => $this -> userName,
+                "password" => $this -> userPassword,
+                "grant_type" => "password",
+                "client_id" => $this -> clientId,
+                "client_secret" => $this -> clientSecret
+            ]);
+    
+            $token = json_decode($tokenResponse -> content(),true);
+            
+            $response = $this->get('/api/v1/validate',
+                [ "Authorization" => "Bearer " . $token ['access_token']]
+            );
+    
+            $response->assertStatus(200);
+        }
+
+        public function test_LogoutNoToken(){
+
+        $response = $this->get('/api/v1/logout');
+
+        $response->assertStatus(500);
+        
+        }
+
+        public function test_LogoutBadRequest(){
+        $response = $this->get('/api/v1/logout',[
+            [ "Authorization" => "Bearer " . Str::Random(40)]
+        ]);
+
+        $response->assertStatus(500);
+        }
+
+        public function test_LogoutGoodRequest(){
+            $tokenResponse = $this->post('/oauth/token',[
+                "username" => $this -> userName,
+                "password" => $this -> userPassword,
+                "grant_type" => "password",
+                "client_id" => $this -> clientId,
+                "client_secret" => $this -> clientSecret
+            ]);
+    
+            $token = json_decode($tokenResponse -> content(),true);
+            
+            $response = $this->get('/api/v1/logout',
+                [ "Authorization" => "Bearer " . $token ['access_token']]
+            );
+    
+            $response->assertStatus(200);
+    
+            $response->assertJsonFragment(
+                ['message' => 'Logout succesful, token revoked']
+            );
         }
 }
