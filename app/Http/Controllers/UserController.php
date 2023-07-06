48ef63cc89dc61922456abcf8a693c62cab28b36
ChinoMandarin <francofedullo22@gmail.com>
2023-07-06 20:52:30 -0300

    Request pfp

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,6 +75,7 @@ class UserController extends Controller
         $User -> gender = $request ->post("gender");
         $User -> email = $request ->post("email");
         $User -> password = $request ->post("password");
+        $User -> profile_pic = $request ->post("profile_pic");
         $User -> description = $request ->post("description");
         $User -> homeland = $request ->post("homeland");
         $User -> residence = $request ->post("residence");
