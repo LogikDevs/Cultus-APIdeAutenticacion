24315df7e7d99aaeba9b975fc671ce1af59c0aa1
ChinoManda <francofedullo22@gmail.com>
2023-09-03 11:31:51 -0300

    Intereses con bearer y con url

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -19,32 +19,42 @@ class LikesController extends Controller
     return response()->json(['interests' => $interests], 200);
     }
 
+    public function ListOtherUserInterest($id){
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(['message' => 'User Not found'], 404);
+        }
+        $interests = $user->interests()->get();
+    return response()->json(['interests' => $interests], 200);
+    }
+
     public function ListInterestUsers($id){
         return likes::all()->where("id_interest",$id);
     }
 
     public function Create(request $request){
         $validation = self::CreateValidation($request);
-        if ($validation->fails())
+        if ($validation->fails()){
         return $validation->errors();
-    
-        return $this -> CreateRequest($request);
+        }
+        $user = (new UserController)->ValidateToken($request);
+        $id_user = $user -> id;
+        return $this -> CreateRequest($request, $id_user);
     }
 
     public function CreateValidation(request $request){
         $validation = Validator::make($request->all(),[
             'id_interest'=>'required | exists:interest_label,id_label',
-            'id_user'=>'required | exists:users,id' 
         ]);
         return $validation;
     }
 
-    public function CreateRequest(request $request)
+    public function CreateRequest(request $request, int $id_user)
     {
         $Likes = new likes();
 
         $Likes -> id_interest = $request ->post("id_interest");
-        $Likes -> id_user = $request ->post("id_user");
+        $Likes -> id_user = $id_user;
         $Likes -> save();
         return $Likes;
     }
