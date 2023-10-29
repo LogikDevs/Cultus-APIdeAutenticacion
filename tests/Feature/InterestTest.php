01c2bcf7f264c9f113f5e4b82e184b7e548ce3de
ChinoManda <francofedullo22@gmail.com>
2023-10-29 15:25:28 -0300

    Interests testing

--- tests/Feature/InterestTest.php
+++ tests/Feature/InterestTest.php
@@ -5,9 +5,33 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 class InterestTest extends TestCase
 {
+    private $BearerToken;
+    
+    public function setUp() :void{
+     parent::setUp();
+
+     $this->userName = getenv("USERNAME");
+     $this->userPassword = getenv("USERPASSWORD");
+     $this->clientId = getenv("CLIENTID");
+     $this->clientSecret = getenv("CLIENTSECRET");
+
+     $tokenHeader = [ "Content-Type" => "application/json"];
+     $Bearer = Http::withHeaders($tokenHeader)->post(getenv("API_AUTH_URL") . "/oauth/token",
+      [
+         'username' => $this->userName,
+         'password' => $this->userPassword,
+         "grant_type" => "password",
+         'client_id' => $this->clientId,
+         'client_secret' => $this->clientSecret,
+     ])->json();
+
+     $this->BearerToken = $Bearer['access_token'];
+     $this->withHeaders(['Authorization' => 'Bearer ' . $this->BearerToken]);
+    }
     public function test_ListAll(){
         $response = $this->get('api/v1/interest');
         $response -> assertStatus(200);
